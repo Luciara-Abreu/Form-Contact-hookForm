acbbf96abca48ed9efc2bc20134e10e6476513d8
Luciara Abreu <luciara.abreu@gmail.com>
2023-07-20 21:43:07 -0300

    feat: aff field array, add send email with api more handleSubmit

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -2,16 +2,17 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { ContainerButton, ContainerFormSendEmail, ContainerSection, ContainerSectionForm } from "./styles";
 import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, useFieldArray } from 'react-hook-form';
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import Thanks from '../tanks';
 
-/**
- * 
+
+/*** 
  * To-do
  * 
  * [ok] Validação / Transformação com Zod
- * [] Field Arrays
+ * [ok] Field Arrays / 
  * [] Upload de arquivos
  * [] Composition Pattern
  * 
@@ -26,25 +27,42 @@ const captalize = ((name: string) => {
 const createContactFormSchema = z.object({
   // Validação com Zod
 
-  name:    z.string().nonempty('O nome é obrigatório').transform(captalize),
-  email:   z.string().nonempty('O email é obrigatório').email('Formato de email inválido').toLowerCase(),
-  fone:    z.string().min(11, 'informe o DDD + o seu numero'),
+  nome: z.string().nonempty('O nome é obrigatório').transform(captalize),
+  email: z.string().nonempty('O email é obrigatório').email('Formato de email inválido').toLowerCase(),
+  fone: z.string().min(11, 'informe o DDD + o seu numero'),
   message: z.string().nonempty('O campo é obrigatório').min(6, 'Precisa ser uma mensagem :D'),
+  area: z.array(z.object({
+    title: z.string().nonempty('Campo Obrgatório'),
+    empresa: z.string().nonempty('Campo Obrgatório'),
+    vaga: z.string(),
+  }))
 })
 
 //typagem
-type CreateContactFormData = z.infer<typeof createContactFormSchema> 
+type CreateContactFormData = z.infer<typeof createContactFormSchema>
 
-function FormSendEmail() {
+// Prop da função de sucesso do formulário
+type FormSubmitSuccessProps = {
+  onFormSubmitSuccess: () => void;
+};
+
+
+function FormSendEmail({ onFormSubmitSuccess }: FormSubmitSuccessProps) {
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [output, setOutput] = useState('')
-  const { register, handleSubmit, formState: { errors } } = useForm<CreateContactFormData>({
+  const { register, handleSubmit, formState: { errors }, control } = useForm<CreateContactFormData>({
     resolver: zodResolver(createContactFormSchema)
   })
 
-  //ver dados do imput 
-  function createMessage(data: any) {
-    console.log(JSON.stringify(data, null, 2))
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'area',
+  })
+
+  function addInformation() {
+    append({ title: '', empresa: '', vaga: '' })
   }
+
 
   useEffect(() => {
     AOS.init(); // Inicia o AOS
@@ -55,28 +73,61 @@ function FormSendEmail() {
   }, []);
 
 
-//<form action="https://formsubmit.co/6022aec38f12017e7094cebed4e0c3bf" method="POST"
+
+  function createMessage(data: any) {
+    fetch("https://formsubmit.co/ajax/6022aec38f12017e7094cebed4e0c3bf", {
+      method: "POST",
+      headers: { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+          name: "FormSubmit",
+          message: JSON.stringify(data, null, 2)
+      })
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Verifica se o formulário foi enviado com sucesso
+    if (data.success) {
+      setIsFormSubmitted(true); // Define o estado para true após o envio do formulário
+      onFormSubmitSuccess();
+      console.log(JSON.stringify(data, null, 2));
+      }
+  })
+  .catch(error => {
+    console.error('Erro na solicitação:', error);
+  });
+}
+
+// Redireciona para a página de agradecimento quando o estado isFormSubmitted for true
+useEffect(() => {
+  if (isFormSubmitted) {
+    window.location.href = "http://localhost:3000/"; // Redirecionamento para a página de agradecimento
+  }
+}, [isFormSubmitted]);
+
 
   return (
     <ContainerFormSendEmail>
       <ContainerSection>
-        <ContainerSectionForm data-aos="zoom-in-left">
+        <ContainerSectionForm data-aos="zoom-in-left"> 
           <form onSubmit={handleSubmit(createMessage)}>
 
             <div className='containerLabel'>
               <label>Nome</label>
               <input type="text" className="form-control" placeholder="nome sobrenome"
-                autoComplete="on" 
-                {...register('name')}
+                autoComplete="on"
+                {...register('nome')}
               />
-              {errors.name && <span>{errors.name.message}</span>}
+              {errors.nome && <span>{errors.nome.message}</span>}
             </div>
 
             <div className='containerGrid'>
               <div className="left">
                 <label>Email</label>
                 <input type="email" className="form-control" placeholder="nome@email.com"
-                  autoComplete="on" 
+                  autoComplete="on"
                   {...register('email')}
                 />
                 {errors.email && <span>{errors.email.message}</span>}
@@ -85,12 +136,56 @@ function FormSendEmail() {
               <div className="right">
                 <label>Fone </label>
                 <input type="fone" className="form-control" placeholder="(xx) xxxxx-xxxx"
-                  autoComplete="on" 
+                  autoComplete="on"
                   {...register('fone')}
                 />
                 {errors.fone && <span>{errors.fone.message}</span>}
               </div>
             </div>
+
+            <div className='containerLabel'>
+              <label className="more-information">
+                <div className='subtitles-information'>
+                <p className="p-information1">Tem uma vaga, e quer adicionar mais informações?</p> 
+                <p className="p-information2">clique aqui</p>
+                </div>
+                <ContainerButton>
+                  <button className="btnSubmit info" type="button" onClick={addInformation}>Adicionar</button>
+                </ContainerButton>
+              </label>
+            </div>
+
+
+            {/**Add os campos para ascrentar mais informações */}
+            {fields.map((field, index) => {
+              return (
+                <div key={field.id} className='containerLabel'>
+                  <div>
+                    <label>Sua área</label>
+                    <input type="text" className="form-control" placeholder="Exemplo Tec Recruter"
+                      {...register(`area.${index}.title`)}
+                    />
+                    {errors.area?.[index]?.title && <span>{errors.area?.[index]?.title?.message}</span>}
+                  </div>
+
+                  <div>
+                    <label>Empresa</label>
+                    <input type="text" className="form-control" placeholder="Exemplo XPTO"
+                      {...register(`area.${index}.empresa`)}
+                    />
+                    {errors.area?.[index]?.empresa && <span>{errors.area?.[index]?.empresa?.message}</span>} 
+                    </div>
+
+                  <div>
+                    <label>Vaga</label>
+                    <input type="text" className="form-control vaga" placeholder="Exemplo Desenvolvedora Front-end"
+                      {...register(`area.${index}.vaga`)}
+                    />
+                    {errors.area?.[index]?.vaga && <span>{errors.area?.[index]?.vaga?.message}</span>}
+                    </div>
+                </div>
+              )
+            })}
 
             <div className='containerLabel'>
               <label >Mensagem</label>
@@ -103,6 +198,8 @@ function FormSendEmail() {
             <ContainerButton>
               <button className="btnSubmit" type="submit">Enviar</button>
             </ContainerButton>
+
+            <input type="hidden" name="redirectTo" value="Thanks" />
           </form>
         </ContainerSectionForm>
       </ContainerSection>
@@ -111,3 +208,4 @@ function FormSendEmail() {
 }
 
 export default FormSendEmail;
+
