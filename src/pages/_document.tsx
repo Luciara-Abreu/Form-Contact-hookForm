70fd554c9b543a9bb80f2521818de14f04f2775d
Luciara Abreu <luciara.abreu@gmail.com>
2023-07-21 22:22:37 -0300

    fix: update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" style={{margin: 0, padding: 0}}>
+    <Html lang="en" style={{margin: 0, padding: 0,  width: '101%'}}>
       <Head />
-      <body style={{margin: 0, padding: 0}}>
+      <body style={{width: '150', margin: 0, padding: 0}}>
         <Main />
         <NextScript />
       </body>
